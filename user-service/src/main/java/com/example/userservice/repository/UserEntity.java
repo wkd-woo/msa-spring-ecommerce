9706edd793ca5b7a25754a33573c4d3399f64418
wkd-woo <wkdwoos@gmail.com>
2021-12-29 23:53:41 +0900

    User Microservice - JPA 1

---
+++ user-service/src/main/java/com/example/userservice/repository/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.userservice.repository;
+
+import lombok.Data;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String encryptedPwd;
+
+
+}
---
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.userservice.service;
+
+
+import com.example.userservice.dto.UserDto;
+
+public interface UserService {
+
+    UserDto createUser(UserDto userDto);
+    
+}
